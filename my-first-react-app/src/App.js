d987f1697b755ae954e265982521f683ee9573eb
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-04-30 10:52:25 -0600

    change variable (component doesn't change)

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,22 +2,17 @@ import logo from './assets/logo.svg'
 import './App.css';
 
 function App() {
+  let name = 'Mario'
+
+  const changeName = () => { 
+    name = 'Luigi'
+    console.log('luigi')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>It's a me {name}</h2>
+      <button onClick={changeName}>Change name</button>
     </div>
   );
 }
