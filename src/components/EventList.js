f05100fb92da48cf6e7d6c9873ed6bdfbd8e1335
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-05-04 00:33:04 -0600

    css modular conditional classNames

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,4 +1,5 @@
 import Title from "./Title"
+import styles from './EventList.module.css'
 
 export default function EventList({ events, handleClick }) {
     return (
@@ -6,7 +7,7 @@ export default function EventList({ events, handleClick }) {
         { events.map((event, index) => (
             <div >
             <Title title={event.name} />
-            <button onClick={ () => handleClick(event.id)}>Remove event</button>
+            <button className={ index == 1 ? styles.button : null } onClick={ () => handleClick(event.id)}>Remove event</button>
             </div>
         )) }
         </div>
