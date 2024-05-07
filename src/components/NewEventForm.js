50d26a8a05c058ee333d80ac8bc8a776a16f0f13
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-05-07 16:47:48 -0600

    on change event, display inputs value

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,18 +1,24 @@
 import './NewEventForm.css'
-
-import React from 'react'
+import { useState } from 'react';
 
 export default function NewEventForm() {
+
+  let [title,setTitle] = useState(null);
+  let [date,setDate] = useState(null)
+  
   return (
     <form className='new-event-form'>
-      <label htmlFor="name-event">
-        <span>Name event</span>
-        <input type="text" name='name-event' />
+      <label htmlFor="title-event">
+        <span>Title event</span>
+        <input type="text" name='title-event' onChange={(e) => { setTitle(e.target.value) }} />
       </label>
       <label htmlFor="date-event">
         <span>Date event</span>
-        <input type="date" name="date-event" />
+        <input type="date" name="date-event" onChange={(e) => { setDate(e.target.value) }} />
       </label>
+      <div>
+        {title} - {date}
+      </div>
       <button type="submit">Submit</button>
     </form>
   )
